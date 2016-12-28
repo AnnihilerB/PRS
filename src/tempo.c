3aa95f4c6acddf1937081960f6863b03d08d4f8c
Ali <cherifiali@outlook.fr>
2016-12-28 10:48:50 +0100

    Temporisateur fonctionnel pour 1 seul objets.

--- src/tempo.c
+++ src/tempo.c
@@ -28,12 +28,14 @@ static unsigned long get_time (void)
 
 #ifdef PADAWAN
 
+// Routine de traitement
 void routine(int sig){
     fprintf (stderr, "sdl_push_event(%p) appelée au temps %ld\n", param_global, get_time ());
 }
 
 void* demon (void* arg){
     
+    //Strucure obligatoire car SIGALRM fait quitter le programme si il n'est pas traité par un handler.
     struct sigaction s;
     s.sa_flags = 0;
     sigemptyset(&s.sa_mask);
@@ -42,14 +44,19 @@ void* demon (void* arg){
     sigaction(SIGALRM, &s, NULL);
     
     sigset_t mask;
+    //Remplissage du masque de signaux pour bloquer tous les signaux.
     sigfillset(&mask);
+    //Suppression de SIGALRM du masque pour qu'il puisse être délivré. Il est le seul signal non bloqué.
     sigdelset(&mask, SIGALRM);
     
     fprintf(stderr, "Thread de pid : %d\n", getpid());
     
+    //Boucle pour traiter les events en continu.
     while(1){
+        //Attente de SIGALRM
         sigsuspend(&mask);
         fprintf(stderr, "Apres sigsuspend");
+        //traitement de l'event
         sdl_push_event(param_global);
         fprintf(stderr, "Apres pushevent");
     }
@@ -60,6 +67,7 @@ int timer_init (void)
 {
     fprintf(stderr, "Timer init\n");
     
+    //Creation d'un masque pour bloquer la transmission du SIGALRM dans le processus
     pthread_t th;
     sigset_t block_mask;
     
@@ -77,6 +85,11 @@ int timer_init (void)
 void timer_set (Uint32 delay, void *param)
 {
     
+    long int sec = delay / 1000;
+    // Recuperation du reste de la division par mille et conversion en micro-secondes
+    long int micro = (delay % 1000) * 1000;
+    
+    fprintf(stderr, "Delay : %d, sec : %ld, micro : %ld\n", delay, sec, micro);
     param_global = param;
     
     struct itimerval it;
@@ -84,7 +97,9 @@ void timer_set (Uint32 delay, void *param)
     struct timeval tv_interval;
     struct timeval tv_value;
     
-    tv_value.tv_sec = delay / 1000;
+    // les deux valeurs sont importantes. pour 2,2 secondes, sec = 2 (en secondes) et micro = 200000 (0,2s converties en micro secondes) 
+    tv_value.tv_sec = sec;
+    tv_value.tv_usec = micro;
     
     it.it_interval = tv_interval;
     it.it_value = tv_value;
